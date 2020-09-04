1f0db9988c6e4fb0aa6c08c4db4bb1b89538bcef
Juma Allan <allanjuma@gmail.com>
2020-09-04 11:23:55 +0300

    Added Theming to BindingActivity.kt

--- app/src/main/java/com/mylivn/ui/views/BindingActivity.kt
+++ app/src/main/java/com/mylivn/ui/views/BindingActivity.kt
@@ -1,14 +1,21 @@
 package com.mylivn.ui.views
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.Observer
+import com.mylivn.ui.viewmodels.ThemeViewModel
+import org.koin.android.ext.android.inject
 
 /**
  * Abstract Activity to create a [ViewDataBinding] for the given type when setting the content view.
  */
 abstract class BindingActivity<T : ViewDataBinding> : AppCompatActivity() {
+
+    private val themeViewModel: ThemeViewModel by inject()
 
     abstract val layoutResId: Int
     lateinit var binding: T
@@ -16,6 +23,25 @@ abstract class BindingActivity<T : ViewDataBinding> : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutResId)
+
+        themeViewModel.getAppTheme().observe(
+            this,
+            Observer {
+                it.let {
+                    when (it) {
+                        "Light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        "Dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        "System" ->
+                            if (Build.VERSION.SDK_INT >= 29) {
+                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                            } else {
+                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                            }
+                    }
+                }
+            }
+        )
+
     }
 
     override fun onDestroy() {
