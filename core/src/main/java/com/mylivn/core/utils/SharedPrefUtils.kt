97c9dd25f9901b069fe52e115a546b4c1ee25d6c
Juma Allan <allanjuma@gmail.com>
2020-09-04 11:12:37 +0300

    added SharedPref Util

---
+++ core/src/main/java/com/mylivn/core/utils/SharedPrefUtils.kt
@@ -0,0 +1,49 @@
+package com.mylivn.core.utils
+
+import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
+
+/**
+ * SharedPreferenceLiveData
+ *
+ * This class allows us to observe the shared preference file, as a live data object.
+ *      - we observe the theme changes, so that the app theme changes without any reload
+ */
+abstract class SharedPreferenceLiveData<T>(
+    val sharedPreferences: SharedPreferences,
+    private val key: String,
+    private val defValue: String
+) : LiveData<T>() {
+
+    private val onSharedPreferenceChangeListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+            if (key == this.key) {
+                value = getValueFromSharedPreferences(key, defValue)
+            }
+        }
+
+    abstract fun getValueFromSharedPreferences(key: String, defValue: String): T
+
+    override fun onActive() {
+        super.onActive()
+        value = getValueFromSharedPreferences(key, defValue)
+        sharedPreferences.registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener)
+    }
+
+    override fun onInactive() {
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(
+            onSharedPreferenceChangeListener
+        )
+        super.onInactive()
+    }
+}
+
+class MylivnSharedPreferenceLiveData(
+    sharedPreferences: SharedPreferences,
+    key: String,
+    defValue: String
+) : SharedPreferenceLiveData<String?>(sharedPreferences, key, defValue) {
+
+    override fun getValueFromSharedPreferences(key: String, defValue: String): String? =
+        sharedPreferences.getString(key, defValue)
+}
