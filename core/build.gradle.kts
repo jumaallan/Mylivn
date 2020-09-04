0167647ede2cc038572e8c6ed46135a4403e9c4b
Juma Allan <allanjuma@gmail.com>
2020-09-04 10:27:18 +0300

    added core library

---
+++ core/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    id(BuildPlugins.androidLibrary)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExtensions)
+    id(BuildPlugins.kapt)
+}
+
+android {
+    compileSdkVersion(AndroidSdk.compileSdkVersion)
+
+    defaultConfig {
+        minSdkVersion(AndroidSdk.minSdkVersion)
+        targetSdkVersion(AndroidSdk.targetSdkVersion)
+        vectorDrawables.useSupportLibrary = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Libraries.kotlinStdLib)
+    implementation(Libraries.coreKtx)
+
+    // Material Design
+    implementation(Libraries.material)
+
+    // Firebase crashlytics
+    implementation(Libraries.crashlytics)
+
+    // Network - Retrofit, OKHTTP
+    implementation(Libraries.retrofit)
+    implementation(Libraries.ohttp)
+    implementation(Libraries.loggingInterceptor)
+    implementation(Libraries.gson)
+
+    // Coroutines
+    implementation(Libraries.coroutines)
+    implementation(Libraries.coroutinesAndroid)
+
+    // DI - KOIN
+    implementation(Libraries.koin)
+    implementation(Libraries.koinViewModel)
+
+    // debug
+    implementation(Libraries.timber)
+
+    // tests
+    testImplementation(TestLibraries.jUnit)
+    androidTestImplementation(TestLibraries.jUnitTest)
+    testImplementation(TestLibraries.truth)
+}
