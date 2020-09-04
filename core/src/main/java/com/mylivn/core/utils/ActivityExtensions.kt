e3efbb4d484ea788499d53beb31f5bf05755f3a4
Juma Allan <allanjuma@gmail.com>
2020-09-04 11:50:21 +0300

    Added Utility classes

---
+++ core/src/main/java/com/mylivn/core/utils/ActivityExtensions.kt
@@ -0,0 +1,46 @@
+package com.mylivn.core.utils
+
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+
+/**
+ * Responsible for making the status bar transparent
+ */
+fun Activity.makeStatusBarTransparent() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            } else {
+                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            }
+            statusBarColor = Color.TRANSPARENT
+        }
+    }
+}
+
+/**
+ * Responsible for creating a toast
+ *
+ * @param message
+ * @param duration
+ */
+fun Activity.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
+}
+
+/**
+ * Responsible for creating a snackBar on an Activity
+ *
+ * @param message
+ */
+fun Activity.makeSnackbar(message: String) {
+    findViewById<View>(android.R.id.content).makeSnackbar(message)
+}
