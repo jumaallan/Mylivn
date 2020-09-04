539ae475ee827c8ecd46518d5b5b9060878fef68
Juma Allan <allanjuma@gmail.com>
2020-09-04 10:43:12 +0300

    Updated Constants with Base URL

---
+++ core/src/main/java/com/mylivn/core/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.mylivn.core.utils
+
+/**
+ *  Constants file
+ *
+ *  This holds the Base URL for the StarWars API
+ */
+object Constants {
+
+    const val BASE_URL = "https://developer.marvel.com/"
+}
