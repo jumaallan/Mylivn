7a5b44eb7fe3446cefd93b619046ba06b6a29165
Juma Allan <allanjuma@gmail.com>
2020-09-06 11:40:22 +0300

    setting up events data flow

--- app/src/main/java/com/mylivn/repository/EventsRepository.kt
+++ app/src/main/java/com/mylivn/repository/EventsRepository.kt
@@ -1,7 +1,20 @@
 package com.mylivn.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.mylivn.data.local.dao.EventsDao
+import com.mylivn.data.local.entities.Events
+import kotlinx.coroutines.flow.Flow
 
 class EventsRepository(
     private val eventsDao: EventsDao
-)
+) {
+
+    fun getHeroEvents(heroId: Int): Flow<PagingData<Events>> =
+        Pager(
+            PagingConfig(pageSize = 40, enablePlaceholders = false, prefetchDistance = 3),
+            pagingSourceFactory = { eventsDao.getHeroEvents(heroId) }
+        ).flow
+
+}
--- app/src/main/java/com/mylivn/ui/viewmodels/EventsViewModel.kt
+++ app/src/main/java/com/mylivn/ui/viewmodels/EventsViewModel.kt
@@ -1,8 +1,17 @@
 package com.mylivn.ui.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.paging.PagingData
+import com.mylivn.data.local.entities.Events
 import com.mylivn.repository.EventsRepository
 
 class EventsViewModel(
     private val eventsRepository: EventsRepository
-) : ViewModel()
+) : ViewModel() {
+
+    fun getHeroEvents(heroId: Int): LiveData<PagingData<Events>> =
+        eventsRepository.getHeroEvents(heroId).asLiveData()
+
+}
