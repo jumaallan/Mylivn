aeb75b7fa439af24ae0feabe720150bd85da9ae5
Juma Allan <allanjuma@gmail.com>
2020-09-06 15:31:13 +0300

    setting up instrumentation test

--- app/src/androidTest/java/com/mylivn/ui/views/HeroActivityTest.kt
+++ app/src/androidTest/java/com/mylivn/ui/views/HeroActivityTest.kt
@@ -2,6 +2,7 @@ package com.mylivn.ui.views
 
 import android.view.View
 import androidx.preference.PreferenceManager
+import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
@@ -9,13 +10,19 @@ import com.agoda.kakao.image.KImageView
 import com.agoda.kakao.recycler.KRecyclerItem
 import com.agoda.kakao.recycler.KRecyclerView
 import com.agoda.kakao.screen.Screen
+import com.agoda.kakao.screen.Screen.Companion.onScreen
 import com.agoda.kakao.text.KTextView
 import com.mylivn.R
+import com.mylivn.core.settings.Settings
+import com.mylivn.core.utils.MylivnSharedPreferenceLiveData
 import com.mylivn.repository.*
 import com.mylivn.ui.viewmodels.*
+import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.Matcher
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.inject
 import org.koin.test.KoinTest
@@ -50,9 +57,40 @@ class HeroActivityTest : KoinTest {
         }
     }
 
+    @Test
+    fun basicHeroInfoIsDisplayed(): Unit = runBlocking {
+
+        every { themeViewModel.getAppTheme() } returns MylivnSharedPreferenceLiveData(
+            sharedPreferences,
+            Settings.PREFERENCE_THEME_KEY,
+            "Light"
+        )
+
+        ActivityScenario.launch(HeroActivity::class.java)
+
+        onScreen<HeroScreen> {
+            textViewBirthYear {
+                isDisplayed()
+                hasText("869BBY")
+            }
+//            planetName {
+//                isDisplayed()
+//                hasText("Tatooine")
+//            }
+//
+//            planetPopulation {
+//                isDisplayed()
+//                hasText("120000")
+//            }
+        }
+        Unit
+        Screen.idle(3000)
+    }
+
 }
 
 class HeroScreen : Screen<HeroScreen>() {
+    val textViewBirthYear = KTextView { withId(R.id.textViewBirthYear) }
     val heroesList = KRecyclerView(
         { withId(R.id.recyclerViewHero) },
         itemTypeBuilder = {
