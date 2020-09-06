b7f8d2a7e27a0550518cf86feda2f114c24d0f29
Juma Allan <allanjuma@gmail.com>
2020-09-06 18:36:22 +0300

    Working Repository tests

--- app/src/test/java/com/mylivn/repository/MarvelRepositoryTest.kt
+++ app/src/test/java/com/mylivn/repository/MarvelRepositoryTest.kt
@@ -1,7 +1,9 @@
 package com.mylivn.repository
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
 import com.mylivn.BaseTest
+import com.mylivn.core.network.NetworkResult
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
@@ -21,7 +23,11 @@ internal class MarvelRepositoryTest : BaseTest() {
     @Test
     fun `test to fetch list of heroes, and their details`() {
         runBlocking {
-
+            val heroResponse =
+                marvelRepository.fetchHeroes()
+            Truth.assertThat(heroResponse).isInstanceOf(NetworkResult.Success::class.java)
+            (heroResponse as NetworkResult.Success)
+            Truth.assertThat(heroResponse.data.data.results[0].name).matches("3-D Man")
         }
     }
 }
