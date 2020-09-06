579f27da96a0d4b14e1c71aaae94d3d5e57a102d
Juma Allan <allanjuma@gmail.com>
2020-09-06 11:45:23 +0300

    setting up series data flow

--- app/src/main/java/com/mylivn/repository/SeriesRepository.kt
+++ app/src/main/java/com/mylivn/repository/SeriesRepository.kt
@@ -1,7 +1,20 @@
 package com.mylivn.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.mylivn.data.local.dao.SeriesDao
+import com.mylivn.data.local.entities.Series
+import kotlinx.coroutines.flow.Flow
 
 class SeriesRepository(
     private val seriesDao: SeriesDao
-)
+) {
+
+    fun getHeroSeries(heroId: Int): Flow<PagingData<Series>> =
+        Pager(
+            PagingConfig(pageSize = 40, enablePlaceholders = false, prefetchDistance = 3),
+            pagingSourceFactory = { seriesDao.getHeroSeries(heroId) }
+        ).flow
+
+}
--- app/src/main/java/com/mylivn/ui/viewmodels/SeriesViewModel.kt
+++ app/src/main/java/com/mylivn/ui/viewmodels/SeriesViewModel.kt
@@ -1,8 +1,17 @@
 package com.mylivn.ui.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.paging.PagingData
+import com.mylivn.data.local.entities.Series
 import com.mylivn.repository.SeriesRepository
 
 class SeriesViewModel(
     private val seriesRepository: SeriesRepository
-) : ViewModel()
+) : ViewModel() {
+
+    fun getHeroSeries(heroId: Int): LiveData<PagingData<Series>> =
+        seriesRepository.getHeroSeries(heroId).asLiveData()
+
+}
