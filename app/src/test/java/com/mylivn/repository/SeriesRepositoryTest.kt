af9b5e26452dcc3d16cc4976a4e5d89dddb39268
Juma Allan <allanjuma@gmail.com>
2020-09-06 13:31:29 +0300

    setting up series repository tests

---
+++ app/src/test/java/com/mylivn/repository/SeriesRepositoryTest.kt
@@ -0,0 +1,27 @@
+package com.mylivn.repository
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.mylivn.BaseTest
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class SeriesRepositoryTest : BaseTest() {
+
+    private lateinit var seriesRepository: SeriesRepository
+
+    @Before
+    fun setUp() {
+        super.setup()
+        seriesRepository = SeriesRepository(seriesDao)
+    }
+
+    @Test
+    fun `test to fetch list of series for a hero`() {
+        runBlocking {
+
+        }
+    }
+}
