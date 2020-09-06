8f572a295694326865d0aa4e6c31e12172d7e258
Juma Allan <allanjuma@gmail.com>
2020-09-06 13:31:37 +0300

    setting up stories repository tests

---
+++ app/src/test/java/com/mylivn/repository/StoriesRepositoryTest.kt
@@ -0,0 +1,27 @@
+package com.mylivn.repository
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.mylivn.BaseTest
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class StoriesRepositoryTest : BaseTest() {
+
+    private lateinit var storiesRepository: StoriesRepository
+
+    @Before
+    fun setUp() {
+        super.setup()
+        storiesRepository = StoriesRepository(storiesDao)
+    }
+
+    @Test
+    fun `test to fetch list of stories for a hero`() {
+        runBlocking {
+
+        }
+    }
+}
