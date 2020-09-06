95f12693eda52f8316599b4c13204c5013637278
Juma Allan <allanjuma@gmail.com>
2020-09-06 11:38:52 +0300

    setting up comics data flow

--- app/src/main/java/com/mylivn/repository/ComicsRepository.kt
+++ app/src/main/java/com/mylivn/repository/ComicsRepository.kt
@@ -1,7 +1,19 @@
 package com.mylivn.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.mylivn.data.local.dao.ComicsDao
+import com.mylivn.data.local.entities.Comics
+import kotlinx.coroutines.flow.Flow
 
 class ComicsRepository(
     private val comicsDao: ComicsDao
-)
+) {
+
+    fun getHeroComics(heroId: Int): Flow<PagingData<Comics>> =
+        Pager(
+            PagingConfig(pageSize = 40, enablePlaceholders = false, prefetchDistance = 3),
+            pagingSourceFactory = { comicsDao.getHeroComics(heroId) }
+        ).flow
+}
--- app/src/main/java/com/mylivn/ui/viewmodels/ComicsViewModel.kt
+++ app/src/main/java/com/mylivn/ui/viewmodels/ComicsViewModel.kt
@@ -1,8 +1,16 @@
 package com.mylivn.ui.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.paging.PagingData
+import com.mylivn.data.local.entities.Comics
 import com.mylivn.repository.ComicsRepository
 
 class ComicsViewModel(
     private val comicsRepository: ComicsRepository
-) : ViewModel()
+) : ViewModel() {
+
+    fun getHeroComics(heroId: Int): LiveData<PagingData<Comics>> =
+        comicsRepository.getHeroComics(heroId).asLiveData()
+}
