ce210791d0dd12b5f4ec88f97bf5d66088e449b5
Juma Allan <allanjuma@gmail.com>
2020-09-06 10:18:13 +0300

    added Room Entities to Database.kt

--- app/src/main/java/com/mylivn/data/local/Database.kt
+++ app/src/main/java/com/mylivn/data/local/Database.kt
@@ -3,11 +3,15 @@ package com.mylivn.data.local
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.mylivn.data.local.dao.HeroDao
-import com.mylivn.data.local.entities.Hero
+import com.mylivn.data.local.entities.*
 
 @androidx.room.Database(
     entities = [
-        Hero::class
+        Hero::class,
+        Comics::class,
+        Events::class,
+        Series::class,
+        Stories::class
     ],
     version = 1,
     exportSchema = false
