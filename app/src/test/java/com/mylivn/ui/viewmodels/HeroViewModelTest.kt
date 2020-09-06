657f37c50b0d66a677670fdbf79901ccc5d856f6
Juma Allan <allanjuma@gmail.com>
2020-09-06 13:17:31 +0300

    setting up hero view model tests

---
+++ app/src/test/java/com/mylivn/ui/viewmodels/HeroViewModelTest.kt
@@ -0,0 +1,27 @@
+package com.mylivn.ui.viewmodels
+
+import com.mylivn.BaseViewModelTest
+import com.mylivn.repository.HeroRepository
+import io.mockk.mockk
+import kotlinx.coroutines.FlowPreview
+import org.junit.Before
+import org.junit.Test
+
+class HeroViewModelTest : BaseViewModelTest() {
+
+    private val heroRepository = mockk<HeroRepository>()
+    private lateinit var heroViewModel: HeroViewModel
+
+    @Before
+    fun setUp() {
+        heroViewModel = HeroViewModel(
+            heroRepository
+        )
+    }
+
+    @FlowPreview
+    @Test
+    fun `test to fetch list of heroes`() {
+
+    }
+}
