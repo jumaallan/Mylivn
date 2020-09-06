3be312e97fe2d039561f36047cd47aba652e1551
Juma Allan <allanjuma@gmail.com>
2020-09-06 11:53:40 +0300

    setting up hero data flow

--- app/src/main/java/com/mylivn/repository/HeroRepository.kt
+++ app/src/main/java/com/mylivn/repository/HeroRepository.kt
@@ -1,7 +1,12 @@
 package com.mylivn.repository
 
 import com.mylivn.data.local.dao.HeroDao
+import com.mylivn.data.local.entities.Hero
 
 class HeroRepository(
     private val heroDao: HeroDao,
-)
+) {
+
+    fun getHero(heroId: Int): Hero =
+        heroDao.getHero(heroId)
+}
--- app/src/main/java/com/mylivn/ui/viewmodels/HeroViewModel.kt
+++ app/src/main/java/com/mylivn/ui/viewmodels/HeroViewModel.kt
@@ -1,8 +1,12 @@
 package com.mylivn.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.mylivn.data.local.entities.Hero
 import com.mylivn.repository.HeroRepository
 
 class HeroViewModel(
     private val heroRepository: HeroRepository
-) : ViewModel()
+) : ViewModel() {
+
+    fun getHero(heroId: Int): Hero = heroRepository.getHero(heroId)
+}
