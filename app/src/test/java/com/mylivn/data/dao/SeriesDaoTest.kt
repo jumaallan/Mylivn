dcff934cbcbe4ce10f1df013396e9f4fdab4be19
Juma Allan <allanjuma@gmail.com>
2020-09-06 13:04:55 +0300

    setting up series dao test

---
+++ app/src/test/java/com/mylivn/data/dao/SeriesDaoTest.kt
@@ -0,0 +1,26 @@
+package com.mylivn.data.dao
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.mylivn.BaseTest
+import com.mylivn.data.sample.seriesSample
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+internal class SeriesDaoTest : BaseTest() {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `test inserting and retrieving series`() = runBlockingTest {
+        seriesDao.insert(seriesSample)
+        val series = seriesDao.getHeroSeries(1)
+        // MatcherAssert.assertThat(comic., `is`(testCharacters[0].name))
+    }
+}
