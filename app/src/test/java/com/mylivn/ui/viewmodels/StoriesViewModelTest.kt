bf39b1ac445d7ded68fb889dcf52da1d7216271b
Juma Allan <allanjuma@gmail.com>
2020-09-06 13:17:57 +0300

    setting up stories view model tests

---
+++ app/src/test/java/com/mylivn/ui/viewmodels/StoriesViewModelTest.kt
@@ -0,0 +1,27 @@
+package com.mylivn.ui.viewmodels
+
+import com.mylivn.BaseViewModelTest
+import com.mylivn.repository.StoriesRepository
+import io.mockk.mockk
+import kotlinx.coroutines.FlowPreview
+import org.junit.Before
+import org.junit.Test
+
+class StoriesViewModelTest : BaseViewModelTest() {
+
+    private val storiesRepository = mockk<StoriesRepository>()
+    private lateinit var storiesViewModel: StoriesViewModel
+
+    @Before
+    fun setUp() {
+        storiesViewModel = StoriesViewModel(
+            storiesRepository
+        )
+    }
+
+    @FlowPreview
+    @Test
+    fun `test to fetch list of stories for a hero`() {
+
+    }
+}
