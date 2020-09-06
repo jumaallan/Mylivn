b9ae29eca9020d8e762316a1e4668db4b7c5b0a0
Juma Allan <allanjuma@gmail.com>
2020-09-06 12:43:01 +0300

    Setting up base unit test

---
+++ app/src/test/java/com/mylivn/BaseTest.kt
@@ -0,0 +1,83 @@
+package com.mylivn
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.google.gson.GsonBuilder
+import com.mylivn.core.data.api.HeroAPI
+import com.mylivn.data.local.Database
+import com.mylivn.data.local.dao.*
+import com.mylivn.dispatcher.MockRequestDispatcher
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+internal open class BaseTest {
+
+    // mock web server and network api
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var okHttpClient: OkHttpClient
+    private lateinit var loggingInterceptor: HttpLoggingInterceptor
+    private lateinit var heroAPI: HeroAPI
+
+    // database and dao
+    private lateinit var database: Database
+    private lateinit var comicsDao: ComicsDao
+    private lateinit var eventsDao: EventsDao
+    private lateinit var heroDao: HeroDao
+    private lateinit var seriesDao: SeriesDao
+    private lateinit var storiesDao: StoriesDao
+
+    @Before
+    open fun setup() {
+        mockWebServer = MockWebServer()
+        mockWebServer.dispatcher = MockRequestDispatcher()
+        mockWebServer.start()
+        loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        okHttpClient = buildOkhttpClient(loggingInterceptor)
+
+        val gson = GsonBuilder()
+            .serializeNulls()
+            .create()
+
+        heroAPI = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(HeroAPI::class.java)
+
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        database =
+            Room.inMemoryDatabaseBuilder(context, Database::class.java).allowMainThreadQueries()
+                .build()
+        comicsDao = database.comicsDao()
+        eventsDao = database.eventsDao()
+        heroDao = database.heroDao()
+        seriesDao = database.seriesDao()
+        storiesDao = database.storiesDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    open fun tearDown() {
+        mockWebServer.shutdown()
+        database.close()
+    }
+
+    private fun buildOkhttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
+}
