05d7571a2411a1bdc9279fa2470b30d941ad8572
Juma Allan <allanjuma@gmail.com>
2020-09-06 12:35:44 +0300

    Setting up Instrumentation tests

---
+++ app/src/androidTest/java/com/mylivn/MylivnTest.kt
@@ -0,0 +1,29 @@
+package com.mylivn
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.error.KoinAppAlreadyStartedException
+import timber.log.Timber
+
+class MylivnTest : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initKoin()
+
+    }
+
+    private fun initKoin() {
+        try {
+            startKoin {
+                androidLogger()
+                androidContext(applicationContext)
+            }
+        } catch (error: KoinAppAlreadyStartedException) {
+            Timber.e(error.localizedMessage)
+        }
+    }
+}
