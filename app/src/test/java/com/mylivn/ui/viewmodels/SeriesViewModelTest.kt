fa2c4492988603eba87958eff5601995fa7e7c7a
Juma Allan <allanjuma@gmail.com>
2020-09-06 13:17:48 +0300

    setting up series view model tests

---
+++ app/src/test/java/com/mylivn/ui/viewmodels/SeriesViewModelTest.kt
@@ -0,0 +1,27 @@
+package com.mylivn.ui.viewmodels
+
+import com.mylivn.BaseViewModelTest
+import com.mylivn.repository.SeriesRepository
+import io.mockk.mockk
+import kotlinx.coroutines.FlowPreview
+import org.junit.Before
+import org.junit.Test
+
+class SeriesViewModelTest : BaseViewModelTest() {
+
+    private val seriesRepository = mockk<SeriesRepository>()
+    private lateinit var seriesViewModel: SeriesViewModel
+
+    @Before
+    fun setUp() {
+        seriesViewModel = SeriesViewModel(
+            seriesRepository
+        )
+    }
+
+    @FlowPreview
+    @Test
+    fun `test to fetch list of series for a hero`() {
+
+    }
+}
