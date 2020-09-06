35d1f260b6e24d013dc70aede332ec76d94fd083
Juma Allan <allanjuma@gmail.com>
2020-09-06 13:31:08 +0300

    setting up hero repository tests

---
+++ app/src/test/java/com/mylivn/repository/HeroRepositoryTest.kt
@@ -0,0 +1,27 @@
+package com.mylivn.repository
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.mylivn.BaseTest
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class HeroRepositoryTest : BaseTest() {
+
+    private lateinit var heroRepository: HeroRepository
+
+    @Before
+    fun setUp() {
+        super.setup()
+        heroRepository = HeroRepository(heroDao)
+    }
+
+    @Test
+    fun `test to fetch a list of heroes`() {
+        runBlocking {
+
+        }
+    }
+}
