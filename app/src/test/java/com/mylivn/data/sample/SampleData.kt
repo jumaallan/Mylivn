b9495a9d4d98929199d6347757f8a98534bd0952
Juma Allan <allanjuma@gmail.com>
2020-09-06 13:02:32 +0300

    creating SampleData

---
+++ app/src/test/java/com/mylivn/data/sample/SampleData.kt
@@ -0,0 +1,33 @@
+package com.mylivn.data.sample
+
+import com.mylivn.data.local.entities.*
+
+val comicsSample = listOf(
+    Comics(0, 1, ""),
+    Comics(0, 1, ""),
+    Comics(0, 1, "")
+)
+
+val eventsSample = listOf(
+    Events(0, 1, ""),
+    Events(0, 1, ""),
+    Events(0, 1, "")
+)
+
+val heroSample = listOf(
+    Hero(0, 1, ""),
+    Hero(0, 1, ""),
+    Hero(0, 1, "")
+)
+
+val seriesSample = listOf(
+    Series(0, 1, ""),
+    Series(0, 1, ""),
+    Series(0, 1, "")
+)
+
+val storiesSample = listOf(
+    Stories(0, 1, ""),
+    Stories(0, 1, ""),
+    Stories(0, 1, "")
+)
