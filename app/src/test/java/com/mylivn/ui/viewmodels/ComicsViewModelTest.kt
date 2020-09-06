f74eba91115a2edb165f151ad5c2040614709471
Juma Allan <allanjuma@gmail.com>
2020-09-06 13:17:17 +0300

    setting up comics view model tests

---
+++ app/src/test/java/com/mylivn/ui/viewmodels/ComicsViewModelTest.kt
@@ -0,0 +1,27 @@
+package com.mylivn.ui.viewmodels
+
+import com.mylivn.BaseViewModelTest
+import com.mylivn.repository.ComicsRepository
+import io.mockk.mockk
+import kotlinx.coroutines.FlowPreview
+import org.junit.Before
+import org.junit.Test
+
+class ComicsViewModelTest : BaseViewModelTest() {
+
+    private val comicsRepository = mockk<ComicsRepository>()
+    private lateinit var comicsViewModel: ComicsViewModel
+
+    @Before
+    fun setUp() {
+        comicsViewModel = ComicsViewModel(
+            comicsRepository
+        )
+    }
+
+    @FlowPreview
+    @Test
+    fun `test to fetch list of comics for a hero`() {
+
+    }
+}
