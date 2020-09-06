61cb9d981352f1d660e76bafb9bacf7b1186bfe2
Juma Allan <allanjuma@gmail.com>
2020-09-06 13:17:24 +0300

    setting up events view model tests

---
+++ app/src/test/java/com/mylivn/ui/viewmodels/EventsViewModelTest.kt
@@ -0,0 +1,27 @@
+package com.mylivn.ui.viewmodels
+
+import com.mylivn.BaseViewModelTest
+import com.mylivn.repository.EventsRepository
+import io.mockk.mockk
+import kotlinx.coroutines.FlowPreview
+import org.junit.Before
+import org.junit.Test
+
+class EventsViewModelTest : BaseViewModelTest() {
+
+    private val eventRepository = mockk<EventsRepository>()
+    private lateinit var eventsViewModel: EventsViewModel
+
+    @Before
+    fun setUp() {
+        eventsViewModel = EventsViewModel(
+            eventRepository
+        )
+    }
+
+    @FlowPreview
+    @Test
+    fun `test to fetch list of events for a hero`() {
+
+    }
+}
