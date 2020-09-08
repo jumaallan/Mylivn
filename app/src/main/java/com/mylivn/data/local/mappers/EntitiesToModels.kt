55ce564a790969095798d281a7c01b8640b891ca
Juma Allan <allanjuma@gmail.com>
2020-09-08 17:55:46 +0300

    Setting up EntitiesToModels Mapper

---
+++ app/src/main/java/com/mylivn/data/local/mappers/EntitiesToModels.kt
@@ -0,0 +1,36 @@
+package com.mylivn.data.local.mappers
+
+import com.mylivn.data.local.entities.*
+import com.mylivn.data.models.*
+
+fun Hero.toModel(): MarvelHero =
+    MarvelHero(
+        heroName = this.heroName,
+        heroDescription = this.heroDescription,
+        heroThumbnail = this.heroThumbnail
+    )
+
+fun Comics.toModel(): HeroComics =
+    HeroComics(
+        comicName = this.comicName,
+        resourceURI = this.resourceURI
+    )
+
+fun Series.toModel(): HeroSeries =
+    HeroSeries(
+        seriesName = this.seriesName,
+        resourceURI = this.resourceURI
+    )
+
+fun Stories.toModel(): HeroStories =
+    HeroStories(
+        storyName = this.storyName,
+        resourceURI = this.resourceURI,
+        type = this.type
+    )
+
+fun Events.toModel(): HeroEvents =
+    HeroEvents(
+        eventName = this.eventName,
+        resourceURI = this.resourceURI,
+    )
