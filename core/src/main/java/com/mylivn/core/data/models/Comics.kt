d31d43e58325beaadeabbaf805529fb06361e81f
Harun Wangereka <harunwangereka@gmail.com>
2020-09-10 14:44:01 +0300

    Fixed comics not saving to room issue

--- core/src/main/java/com/mylivn/core/data/models/Comics.kt
+++ core/src/main/java/com/mylivn/core/data/models/Comics.kt
@@ -3,6 +3,6 @@ package com.mylivn.core.data.models
 data class Comics(
     val available: Int?,
     val collectionURI: String?,
-    val itemComics: List<ItemComics?>?,
+    val items: List<ItemComics?>?,
     val returned: Int?
 )
